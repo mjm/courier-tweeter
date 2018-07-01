ce53ad41d8ce693d55ef5b58ae77e8a5c4abbdca
Matt Moriarity <matt@mattmoriarity.com>
2018-07-01 17:44:13 -0500

    Remove failing test

--- spec/courier/tweeter_spec.rb
+++ spec/courier/tweeter_spec.rb
@@ -1,9 +1,5 @@
 RSpec.describe Courier::Tweeter do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Courier::Tweeter::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
