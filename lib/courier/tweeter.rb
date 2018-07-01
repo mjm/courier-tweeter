4f12b2d85854631a88624e032346c5d9b8122cb3
Matt Moriarity <matt@mattmoriarity.com>
2018-07-01 17:38:10 -0500

    Commit empty gem

---
+++ lib/courier/tweeter.rb
@@ -0,0 +1,7 @@
+require "courier/tweeter/version"
+
+module Courier
+  module Tweeter
+    # Your code goes here...
+  end
+end
---
+++ lib/courier/tweeter/version.rb
@@ -0,0 +1,5 @@
+module Courier
+  module Tweeter
+    VERSION = "0.1.0"
+  end
+end
