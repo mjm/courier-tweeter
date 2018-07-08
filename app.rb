621a32b6a06d7f35eb7397d89e1eb35d1e657395
Matt Moriarity <matt@mattmoriarity.com>
2018-07-08 18:10:51 -0500

    Pull out some environment loading

--- app.rb
+++ app.rb
@@ -3,22 +3,7 @@ require 'omniauth'
 require 'omniauth-twitter'
 require 'twitter'
 
-RACK_ENV = (ENV['RACK_ENV'] || 'development').to_sym
-
 def load_environment
-  if RACK_ENV == :production
-    # load production environment settings
-    require 'google/cloud/storage'
-    storage = Google::Cloud::Storage.new
-    bucket = storage.bucket ENV['GOOGLE_CLOUD_STORAGE_BUCKET']
-    file = bucket.file '.envrc'
-    downloaded = file.download
-    downloaded.rewind
-    downloaded.each_line do |line|
-      ENV[$1] = $2 if line =~ /^export (\w+)="(.*)"$/
-    end
-  end
-
   require 'config/environment'
   Dir['app/middlewares/*.rb'].each { |middleware| require middleware }
   Dir['app/helpers/*.rb'].each { |helper| require helper }
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,19 @@
 require 'sequel'
 
+RACK_ENV = (ENV['RACK_ENV'] || 'development').to_sym
 DB = Sequel.connect(ENV['DB_URL'])
 
 autoload :User, 'app/models/user'
+
+if RACK_ENV == :production
+  # load production environment settings
+  require 'google/cloud/storage'
+  storage = Google::Cloud::Storage.new
+  bucket = storage.bucket ENV['GOOGLE_CLOUD_STORAGE_BUCKET']
+  file = bucket.file '.envrc'
+  downloaded = file.download
+  downloaded.rewind
+  downloaded.each_line do |line|
+    ENV[$1] = $2 if line =~ /^export (\w+)="(.*)"$/
+  end
+end
